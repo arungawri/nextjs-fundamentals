0fae27edc1b446faa2b51ca9ee3c2d397296d362
Arun Gawri <luckygawri@gmail.com>
2023-09-11 12:29:14 +0530

    Fake Store API for SSR example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
  
-type Repo = {
-  name: string
-  stargazers_count: number
+type Product = {
+  id: number;
+	title: string;
+	price: number;
+	description: string;
+	category: string;
+	image: string;
+	rating: { rate: number; count: number; }
 }
- 
+
 export const getServerSideProps: GetServerSideProps<{
-  repo: Repo
+  repo: Array<Product>
 }> = async () => {
-  const res = await fetch('https://api.github.com/repos/vercel/next.js')
+  const res = await fetch('https://fakestoreapi.com/products')
   const repo = await res.json()
   console.log('repo', repo)
   return { props: { repo } }
@@ -17,5 +22,12 @@ export const getServerSideProps: GetServerSideProps<{
 export default function Page({
   repo,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  return repo.stargazers_count
+  return (
+    <main style={{display: "flex", height: "100vh", justifyContent: "center", alignItems: "center"}}>
+      {repo.map(item => <div key={item.id} >
+        {item.title}
+      </div>
+        )}
+    </main>
+  )
 }
